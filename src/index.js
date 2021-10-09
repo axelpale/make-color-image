2fa9e233bf62aefbbb6a220249c3599a566644e5
Akseli Palén <akseli.palen@gmail.com>
2021-10-09 16:32:37 +0300

    Minimal viable implementation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,28 @@
 // Include app css via style loader
 require('./style.css')
 
-// Your code
-const container = document.querySelector('#container')
-container.innerHTML = 'Hello app, this is JS'
+const widthInput = document.querySelector('#imgwidth')
+const heightInput = document.querySelector('#imgheight')
+const colorInput = document.querySelector('#imgcolor')
+const submitBtn = document.querySelector('#imgsubmit')
+
+const canvas = document.querySelector('#imgcanvas')
+const ctx = canvas.getContext('2d')
+
+const updateCanvas = function () {
+  // Read form
+  const w = parseInt(widthInput.value)
+  const h = parseInt(heightInput.value)
+  const c = colorInput.value
+  // Reset canvas size
+  canvas.width = w
+  canvas.height = h
+  // Fill canvas with color
+  ctx.fillStyle = c
+  ctx.fillRect(0, 0, w, h)
+}
+
+submitBtn.addEventListener('click', updateCanvas)
+
+// Initial reset
+updateCanvas()
